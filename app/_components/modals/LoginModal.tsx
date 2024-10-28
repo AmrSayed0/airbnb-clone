726122c9e24297c2c85b7f2d606ddaf1811fafdc
AmrSayed0 <amracademycode@gmail.com>
2024-10-29 01:56:42 +0300

    fix: trigger signin modal post-signup

--- app/_components/modals/LoginModal.tsx
+++ app/_components/modals/LoginModal.tsx
@@ -63,7 +63,7 @@ const LoginModal = () => {
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading title="Welcome Back!" subtitle="Login to continue" center />
+      <Heading title="Welcome to Airbnb" subtitle="Login to continue" center />
 
       <Input
         id="email"
